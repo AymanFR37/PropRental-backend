8cbf7b4732e826b553da4f6346f260ef8b0ae067
Ayman Farik <ayman.farik@gmail.com>
2024-10-24 21:54:16 +0100

    first commit

---
+++ src/main/java/com/backend/proprental/PropRentalApplication.java
@@ -0,0 +1,13 @@
+package com.backend.proprental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/proprental/PropRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.proprental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
